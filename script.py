e4a05134c3f6c73b71e7b27f2ab7b97fc734578b
Shiva <61221829+ShivaMMurugan@users.noreply.github.com>
2021-05-05 14:21:57 +0530

    Update script.py

--- script.py
+++ script.py
@@ -34,7 +34,7 @@ os.popen('cp -r {}/{} tmp'.format(files_original, return_val))
 def remove_folder(folder_name):
 	shutil.rmtree(folder_name)
 # remove_folder(base)
-shutil.rmtree(base)
+# shutil.rmtree(base)
 # with open(os.path.join(base, return_val), 'r') as f:
 # 	data = json.load(f)
 # 	print(data)
